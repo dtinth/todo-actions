8401d92301e04af2339c9c4aef435bcea3d5c511
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-07 02:49:18 +0700

    Find files with TODO marker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 console.log('Files in repo')
 console.log(
   require('child_process')
-    .execSync('git ls-files', { encoding: 'utf8' })
+    .execSync('git grep -Il TODO:', { encoding: 'utf8' })
     .split('\n'),
 )
