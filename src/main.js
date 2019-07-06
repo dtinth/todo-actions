f022e6b0311716fa534a6d96a9b65fca0e909c25
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-07 02:51:51 +0700

    Strip empty string

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ console.log('Files in repo')
 console.log(
   require('child_process')
     .execSync('git grep -Il TODO:', { encoding: 'utf8' })
-    .split('\n'),
+    .split('\n')
+    .filter(name => name),
 )
