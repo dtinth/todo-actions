3b07a7e2ab8165fe8c5d780c86e1dcba80e126c3
dtinth <dtinth@spacet.me>
2019-07-07 09:31:14 +0000

    Put them together

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-console.log('Files containing TODO marker:')
-console.log(
-  require('child_process')
-    .execSync('git grep -Il TODO:', { encoding: 'utf8' })
-    .split('\n')
-    .filter(name => name),
-)
+const { File } = require('./File')
+const { parseTodos } = require('./TodoParser')
+const filesWithTodoMarker = require('child_process')
+  .execSync('git grep -Il TODO:', { encoding: 'utf8' })
+  .split('\n')
+  .filter(name => name)
+
+const todoComments = []
+
+console.log('Parsing TODO tags:')
+for (const filePath of filesWithTodoMarker) {
+  // TODO: Implement ignoring paths
+
+  const file = new File(filePath)
+  const todos = parseTodos(file)
+  console.log('- %s: %s found', filePath, todos.length)
+  todoComments.push(...todos)
+}
+
+console.log('Total: %s found', todoComments.length)
