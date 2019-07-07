fa24de44da351a5bd0ecf8161e4a1b08e105f250
dtinth <dtinth@spacet.me>
2019-07-07 09:14:50 +0000

    Add marker detection

--- src/TodoParser.js
+++ src/TodoParser.js
@@ -1,1 +1,19 @@
-exports.parseTodos = function() {}
+/**
+ * @typedef {object} ITodo
+ */
+
+/**
+ * @param {import('./File').IFile} file
+ * @returns {ITodo[]}
+ */
+exports.parseTodos = function(file) {
+  /** @type {ITodo[]} */
+  const out = []
+  for (const line of file.lines) {
+    const match = line.match(/^(\W+\s)TODO:/)
+    if (match) {
+      out.push({})
+    }
+  }
+  return out
+}
--- src/TodoParser.test.js
+++ src/TodoParser.test.js
@@ -1,7 +1,53 @@
 const { parseTodos } = require('./TodoParser')
+const { MockFile } = require('./File')
+
+const MARKER = 'TODO'
 
 describe('parseTodos', () => {
   it('is a function', () => {
     expect(parseTodos).toBeInstanceOf(Function)
+  })
+
+  it('can parse todo', () => {
+    const file = new MockFile(
+      'main.js',
+      `
+        // ${MARKER}: Item 1
+
+        // ${MARKER}: Item 2
+        // Body
+
+        // ${MARKER}: Item 3
+        //
+        // Extended body
+
+        // Not part of TODO
+
+        /*
+         * ${MARKER}: Item 4
+         * Body
+         *
+         * Extended body
+         */
+
+        <!--
+          - ${MARKER}: Item 5
+          - Body
+          -
+          - Extended body
+
+          Not part of TODO
+          -->
+        
+          # ${MARKER}: Item 6
+          # Body
+          #
+          # Extended body
+          #-
+          # Not part of TODO
+      `,
+    )
+    const result = parseTodos(file)
+    expect(result).toHaveLength(6)
   })
 })
