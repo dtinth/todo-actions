6c603186f1c544a78f2299760185ac19e057550f
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-17 00:50:30 +0700

    Create integration test stub

---
+++ src/TodoActionsMain.test.ts
@@ -0,0 +1,12 @@
+// TODO: Add integration test for TodoActionsMain.
+//
+// Code that interface with external data have been separated into their own modules.
+// These includes:
+//
+// - `DataStore`
+// - `CodeRepository`
+// - `TaskManagementSystem`
+//
+// They can be mocked by creating a mock version using `__mocks__` folder.
+// https://jestjs.io/docs/en/manual-mocks
+it('works')
