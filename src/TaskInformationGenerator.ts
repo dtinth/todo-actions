2ce010753d6546c3973bfe6adbb9ae13cb7bbf93
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-17 00:36:00 +0700

    Don’t hardcode master

--- src/TaskInformationGenerator.ts
+++ src/TaskInformationGenerator.ts
@@ -12,13 +12,14 @@ export function generateTaskInformationFromTodo(todo: ITodo): TaskInformation {
   const title = todo.title
 
   const file = todo.file.fileName
-  // TODO [#31]: Link to end line
+  // TODO [#31]: Also link to end line in addition to just the starting line.
+  // This requires changing `IFile` interface and `File` class to also keep track of where the TODO comment ends.
   const line = todo.startLine
   const owner = repoContext.repositoryOwner
   const repo = repoContext.repositoryName
+  const defaultBranch = repoContext.defaultBranch
 
-  // TODO [#33]: Don’t hardcode master branch when generating URL
-  const url = `https://github.com/${owner}/${repo}/blob/master/${file}#L${line}`
+  const url = `https://github.com/${owner}/${repo}/blob/${defaultBranch}/${file}#L${line}`
   const link = `[${file}:${line}](${url})`
   const body = [
     todo.body,
@@ -26,8 +27,7 @@ export function generateTaskInformationFromTodo(todo: ITodo): TaskInformation {
     '---',
     `_` +
       `This issue has been automatically created by [todo-actions](https://github.com/apps/todo-actions) based on a TODO comment found in ${link}. ` +
-      // TODO [#34]: Don’t hardcode master branch in task description
-      `It will automatically be closed when the TODO comment is removed from the master branch.` +
+      `It will automatically be closed when the TODO comment is removed from the default branch (${defaultBranch}).` +
       `_`,
   ].join('\n')
 
