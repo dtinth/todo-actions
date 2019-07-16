7cf4402ad6bc1de690ba7e96fd71617386cf33b7
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-16 23:57:01 +0700

    Add TaskInformationGenerator

---
+++ src/TaskInformationGenerator.ts
@@ -0,0 +1,26 @@
+import { ITodo, ITaskState } from './types'
+import { createHash } from 'crypto'
+
+type TaskInformation = {
+  state: ITaskState
+  title: string
+  body: string
+}
+
+export function generateTaskInformationFromTodo(todo: ITodo): TaskInformation {
+  const title = todo.title
+
+  // TODO: Add link to source file in body.
+  const body = todo.body
+
+  return {
+    state: {
+      hash: createHash('md5')
+        .update(title)
+        .update(body)
+        .digest('hex'),
+    },
+    title,
+    body,
+  }
+}
