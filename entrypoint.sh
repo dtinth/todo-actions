4a4aba0943bb8deb67a6df94d7408e1b61f2b5c4
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-17 00:44:42 +0700

    Use the compiled JS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-# TODO [#21]: Once precompilation is done, run the compiled script directly.
-#
-# ```
-# sh -c "node /app/lib/main.js $*"
-# ```
-#
-sh -c "/app/node_modules/.bin/ts-node /app/src/CLIEntrypoint.ts $*"
+sh -c "node /app/lib/CLIEntrypoint.js $*"
